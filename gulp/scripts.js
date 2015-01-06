5ca2621e91b8c2d0cf4fa0fabb1599f24360de84
Jeremy <jball@invaluable.com>
2015-01-06 13:07:51 -0500

    Making a separate standalone package

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,30 +1,33 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var exposify = require('exposify');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglifyjs');
 var rename = require('gulp-rename');
 
 module.exports = function() {
 
-  exposify.config = {
-      underscore: '_'
-  }
-
   var browserified = transform(function(filename) {
     var b = browserify(filename, {
       standalone: 'qStringUtils'
+      , bundleExternal: false
     })
-    .transform(exposify);
     return b.bundle();
   });
-  
-  return gulp.src(['./src/qstring.js'])
+
+  gulp.src(['./src/qstring.js'])
     .pipe(browserified)
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.standalone.min.js'
+    }))
+    .pipe(gulp.dest('./'));
+
+  gulp.src(['./src/qstring.js'])
     .pipe(gulp.dest('./'))
     .pipe(uglify())
     .pipe(rename({
       extname: '.min.js'
     }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
+
 }
